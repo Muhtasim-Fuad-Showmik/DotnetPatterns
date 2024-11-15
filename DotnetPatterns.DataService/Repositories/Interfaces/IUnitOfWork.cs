738fa6f0b2c5918b409439c5d5d8c55d815b1570
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-11-15 15:50:28 +0600

    feat: add an interface for "Unit Of Work"

---
+++ DotnetPatterns.DataService/Repositories/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+namespace DotnetPatterns.DataService.Repositories.Interfaces;
+
+public interface IUnitOfWork
+{
+    IDriverRepository Drivers { get; }
+    IAchievementsRepository Achievements { get; }
+
+    Task CompleteAsync();
+}
