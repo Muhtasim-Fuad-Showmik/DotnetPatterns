e59821e5ea99cd3eea91988e4cd3bc5970969c83
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-11-16 03:45:24 +0600

    feat: create all APIs for getting individual and all drivers, creatomg, updating, and deleting specific drivers

--- DotnetPatterns.Api/Controllers/DriversController.cs
+++ DotnetPatterns.Api/Controllers/DriversController.cs
@@ -1,5 +1,9 @@
 using AutoMapper;
 using DotnetPatterns.DataService.Repositories.Interfaces;
+using DotnetPatterns.Entities.DbSet;
+using DotnetPatterns.Entities.Dtos.Requests;
+using DotnetPatterns.Entities.Dtos.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetPatterns.Api.Controllers;
 
@@ -7,5 +11,70 @@ public class DriversController : BaseController
 {
     public DriversController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
     {
+    }
+    
+    [HttpGet]
+    [Route("{driverId:guid}")]
+    public async Task<IActionResult> GetDriver(Guid driverId)
+    {
+        var driver = await _unitOfWork.Drivers.GetById(driverId);
+
+        if (driver == null)
+            return NotFound();
+
+        var result = _mapper.Map<GetDriverResponse>(driver);
+
+        return Ok(result);
+    }
+    
+    [HttpGet]
+    public async Task<IActionResult> GetAllDrivers()
+    {
+        var driver = await _unitOfWork.Drivers.All();
+
+        return Ok(_mapper.Map<IEnumerable<GetDriverResponse>>(driver));
+    }
+
+    [HttpPost("")]
+    public async Task<IActionResult> AddDriver([FromBody] CreateDriverRequest driver)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var result = _mapper.Map<Driver>(driver);
+
+        await _unitOfWork.Drivers.Add(result);
+        await _unitOfWork.CompleteAsync();
+
+        return CreatedAtAction(nameof(GetDriver), new {driverId = result.Id}, result);
+    }
+    
+    [HttpPut("")]
+    public async Task<IActionResult> UpdateDriver([FromBody] UpdateDriverRequest driver)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest();
+
+        var result = _mapper.Map<Driver>(driver);
+
+        await _unitOfWork.Drivers.Update(result);
+        await _unitOfWork.CompleteAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete]
+    [Route("{driverId:guid}")]
+    public async Task<IActionResult> DeleteDriver(Guid driverId)
+    {
+        var driver = await _unitOfWork.Drivers.GetById(driverId);
+
+        if (driver == null)
+            return NotFound();
+
+        await _unitOfWork.Drivers.Delete(driverId);
+        await _unitOfWork.CompleteAsync();
+
+        return NoContent();
     }
 }
