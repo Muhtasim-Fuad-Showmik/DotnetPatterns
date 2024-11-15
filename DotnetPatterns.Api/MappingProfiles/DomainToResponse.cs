9bb5fd8cc8abc1cebd846e892c169232a2b0e5d2
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-11-16 03:06:57 +0600

    feat: implement the DomainToResponse.cs mapping profile

--- DotnetPatterns.Api/MappingProfiles/DomainToResponse.cs
+++ DotnetPatterns.Api/MappingProfiles/DomainToResponse.cs
@@ -1,8 +1,17 @@
 using AutoMapper;
+using DotnetPatterns.Entities.DbSet;
+using DotnetPatterns.Entities.Dtos.Responses;
 
 namespace DotnetPatterns.Api.MappingProfiles;
 
 public class DomainToResponse : Profile
 {
-    
+    public DomainToResponse()
+    {
+        CreateMap<Achievement, DriverAchievementResponse>()
+            .ForMember(
+                dest => dest.Wins,
+                opt => opt.MapFrom(src => src.RaceWins))
+            ;
+    }
 }
