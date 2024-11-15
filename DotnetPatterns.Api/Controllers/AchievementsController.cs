9d73ac7209d45d4e8e979bdd704c079b288bfb17
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-11-15 17:41:57 +0600

    refactor: format AchievementsController.cs to fit parameters neatly

--- DotnetPatterns.Api/Controllers/AchievementsController.cs
+++ DotnetPatterns.Api/Controllers/AchievementsController.cs
@@ -5,7 +5,9 @@ namespace DotnetPatterns.Api.Controllers;
 
 public class AchievementsController : BaseController
 {
-    public AchievementsController(IUnitOfWork unitOfWork, IMapper mapper) : base(unitOfWork, mapper)
+    public AchievementsController(
+        IUnitOfWork unitOfWork, 
+        IMapper mapper) : base(unitOfWork, mapper)
     {
     }
 }
