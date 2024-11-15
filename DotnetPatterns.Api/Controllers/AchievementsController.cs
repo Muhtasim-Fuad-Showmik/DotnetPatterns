6d909980503fcb4de1b96180f3d90f10832a4233
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2024-11-16 03:09:20 +0600

    feat: create route for getting the information for a specific driver

--- DotnetPatterns.Api/Controllers/AchievementsController.cs
+++ DotnetPatterns.Api/Controllers/AchievementsController.cs
@@ -1,5 +1,7 @@
 using AutoMapper;
 using DotnetPatterns.DataService.Repositories.Interfaces;
+using DotnetPatterns.Entities.Dtos.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DotnetPatterns.Api.Controllers;
 
@@ -9,5 +11,19 @@ public class AchievementsController : BaseController
         IUnitOfWork unitOfWork, 
         IMapper mapper) : base(unitOfWork, mapper)
     {
+    }
+
+    [HttpGet]
+    [Route("{driverId:guid}")]
+    public async Task<IActionResult> GetDriverAchievements(Guid driverId)
+    {
+        var driverAchievements = await _unitOfWork.Achievements.GetDriverAchievementAsync(driverId);
+
+        if (driverAchievements == null)
+            return NotFound("Achievements not found");
+
+        var result = _mapper.Map<DriverAchievementResponse>(driverAchievements);
+
+        return Ok(result);
     }
 }
